bf4c81ca4f4c2a72c8447454b1a05ccc8d628a23
schmelztiegel <Melanietonke@gmail.com>
2026-08-30 17:23:15 +0200

    Add files via upload

---
+++ import2026.js
@@ -0,0 +1,98 @@
+window.import2026 = [
+  {
+    "month": "2026-01",
+    "waterReading": 116,
+    "powerReading": 18036,
+    "trashCount": 2,
+    "waterUsage": 0,
+    "powerUsage": 0,
+    "tariffLabel": "Alter Tarif (bis 01.05.2026)",
+    "total": 0,
+    "mel": 0,
+    "diana": 0
+  },
+  {
+    "month": "2026-02",
+    "waterReading": 125,
+    "powerReading": 19417,
+    "trashCount": 1,
+    "waterUsage": 9,
+    "powerUsage": 1381,
+    "tariffLabel": "Alter Tarif (bis 01.05.2026)",
+    "total": 12 + 11.45 + 10 + 6.46 + (9*1.85) + (9*2.56) + (1381*0.23) + (1*5.5),
+    "mel": 0,
+    "diana": 0
+  },
+  {
+    "month": "2026-03",
+    "waterReading": 132,
+    "powerReading": 20340,
+    "trashCount": 1,
+    "waterUsage": 7,
+    "powerUsage": 923,
+    "tariffLabel": "Alter Tarif (bis 01.05.2026)",
+    "total": 12 + 11.45 + 10 + 6.46 + (7*1.85) + (7*2.56) + (923*0.23) + (1*5.5),
+    "mel": 0,
+    "diana": 0
+  },
+  {
+    "month": "2026-04",
+    "waterReading": 138,
+    "powerReading": 21142,
+    "trashCount": 1,
+    "waterUsage": 6,
+    "powerUsage": 802,
+    "tariffLabel": "Alter Tarif (bis 01.05.2026)",
+    "total": 12 + 11.45 + 10 + 6.46 + (6*1.85) + (6*2.56) + (802*0.23) + (1*5.5),
+    "mel": 0,
+    "diana": 0
+  },
+  {
+    "month": "2026-05",
+    "waterReading": 148,
+    "powerReading": 21923,
+    "trashCount": 1,
+    "waterUsage": 10,
+    "powerUsage": 781,
+    "tariffLabel": "Neuer Tarif (ab 02.05.2026)",
+    "total": 14.85 + 11.45 + 10 + 6.46 + (10*1.85) + (10*2.56) + (781*0.2853) + (1*5.5),
+    "mel": 0,
+    "diana": 0
+  },
+  {
+    "month": "2026-06",
+    "waterReading": 154,
+    "powerReading": 456,
+    "trashCount": 1,
+    "waterUsage": 6,
+    "powerUsage": 456,
+    "tariffLabel": "Neuer Tarif (ab 02.05.2026)",
+    "total": 14.85 + 11.45 + 10 + 6.46 + (6*1.85) + (6*2.56) + (456*0.2853) + (1*5.5),
+    "mel": 0,
+    "diana": 0
+  },
+  {
+    "month": "2026-07",
+    "waterReading": 161,
+    "powerReading": 783,
+    "trashCount": 2,
+    "waterUsage": 7,
+    "powerUsage": 327,
+    "tariffLabel": "Neuer Tarif (ab 02.05.2026)",
+    "total": 14.85 + 11.45 + 10 + 6.46 + (7*1.85) + (7*2.56) + (327*0.2853) + (2*5.5),
+    "mel": 0,
+    "diana": 0
+  },
+  {
+    "month": "2026-08",
+    "waterReading": 167,
+    "powerReading": 1090,
+    "trashCount": 1,
+    "waterUsage": 6,
+    "powerUsage": 307,
+    "tariffLabel": "Neuer Tarif (ab 02.05.2026)",
+    "total": 14.85 + 11.45 + 10 + 6.46 + (6*1.85) + (6*2.56) + (307*0.2853) + (1*5.5),
+    "mel": 0,
+    "diana": 0
+  }
+];
